531c36f8c8cfe95d616fc09afe2c6db90b750766
YASMEENELSHAMY <147400556+YASMEENELSHAMY@users.noreply.github.com>
2025-03-29 00:44:42 +0200

    Update header.php

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>registration form</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <header>
+        <div class="header-container">
+            <div class="logo-container">
+            <a href="index.php" class="logo">
+                <img src="logo.png" class="logo-img">
+            </a>
+            </div>
+            <div class="header-title-div">
+            
+            <h1 class="headerTitle"> Register Now & Unlock Benefits </h1>
+            </div>
+        </div>
+    </header>
