9396fff863bcc5632df7849354c52e29ddd8d045
tasneem821 <148282460+TASnim4555@users.noreply.github.com>
2025-06-05 12:57:56 +0300

    finish server side validation

--- RegisterationFormLaravel/app/Http/Controllers/RegisteredUsersController.php
+++ RegisterationFormLaravel/app/Http/Controllers/RegisteredUsersController.php
@@ -9,26 +9,44 @@ use Illuminate\Support\Facades\Hash;
 class RegisteredUsersController extends Controller
 {
     public function store(Request $request)
-        {
+    {
+        // 1. VALIDATION
+        $validated = $request->validate([
+            'fullname' => 'required|string|max:255',
+            'username' => 'required|string|max:255|unique:registered_users',
+            'phone' => 'required|string|max:20',
+            'whats' => 'nullable|string|max:20',
+            'address' => 'required|string|max:500',
+   'password' => [
+        'required',
+        'string',
+        'min:8',
+        'regex:/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]+$/'
+    ],            'email' => 'required|email|max:255|unique:registered_users',
+            'imageUpload' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        // 2. FILE UPLOAD HANDLING
+        $filename = null;
+        if ($request->hasFile('imageUpload')) {
             $file = $request->file('imageUpload');
-            $filename = null;
-
-            if ($file) {
-                $filename = time() . '_' . $file->getClientOriginalName();
-                $file->move(public_path('uploads'), $filename);
-            }
-
-            RegisteredUsers::create([
-                'fullname'         => $request->input('fullname'),
-                'username'         => $request->input('username'),
-                'phone'            => $request->input('phone'),
-                'whats'            => $request->input('whats'),
-                'address'          => $request->input('address'),
-                'password'         => Hash::make($request->input('password')),
-                'email'            => $request->input('email'),
-                'imageUpload'      => $filename,
-            ]);
-
-            return back()->with('success', 'User registered successfully!');
+            $filename = time().'_'.$file->getClientOriginalName();
+            $file->move(public_path('uploads'), $filename);
         }
+
+        // 3. CREATE USER
+        RegisteredUsers::create([
+            'fullname' => $validated['fullname'],
+            'username' => $validated['username'],
+            'phone' => $validated['phone'],
+            'whats' => $validated['whats'] ?? null,
+            'address' => $validated['address'],
+            'password' => Hash::make($validated['password']),
+            'email' => $validated['email'],
+            'imageUpload' => $filename,
+        ]);
+
+        // 4. REDIRECT WITH SUCCESS
+        return back()->with('success', 'User registered successfully!');
+    }
 }
---
+++ RegisterationFormLaravel/storage/framework/views/cf8d9a7a0f10b51140dd50269b2ccc09.php
@@ -0,0 +1,14 @@
+<footer>
+    <div class="footerContainer">
+        <div class="socialIcons">
+            <a href="#"><i class="fa fa-facebook"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="#"><i class="fa fa-instagram"></i></a>
+            <a href="https://www.youtube.com/"><i class="fa fa-youtube"></i></a>
+        </div>
+        <div class="footerText">
+            <p>Copyright ©️ 2025 Registration Form. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
+<?php /**PATH C:\Users\tasnim\Desktop\third-lvl\second-term\User-Registration-webpage\RegisterationFormLaravel\resources\views/partials/footer.blade.php ENDPATH**/ ?>
