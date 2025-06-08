d4c4143d1c247ee7f2933aa51a8bf68b75b22cbb
YASMEEN <147400556+YASMEENELSHAMY@users.noreply.github.com>
2025-06-08 17:32:39 +0300

    Update app.blade.php

--- RegisterationFormLaravel/resources/views/layouts/app.blade.php
+++ RegisterationFormLaravel/resources/views/layouts/app.blade.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <title>{{ __('messages.register_title') }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+    @if(app()->getLocale() == 'ar')
+    <link href="{{ asset('css/rtl.css') }}" rel="stylesheet">
+    @endif
 </head>
 <body>
     @include('partials.header')
